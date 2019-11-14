3aac4938c7287edea49d0c8ad0afad9cce20846d
Pastilhas <joao.campos1221@gmail.com>
2019-11-14 17:33:21 +0000

    caguei

--- src/display.pl
+++ src/display.pl
@@ -1,20 +1,20 @@
-display_board(B):-display_board(B,9).
-display_board(_,0):-write('+---+---+---+---+---+---+---+---+---+'),nl.
-display_board([L|T],I):-
+display_board(B):-display_board(B, 9).
+display_board(_, 0):-write('+---+---+---+---+---+---+---+---+---+'), nl.
+display_board([L|T], I):-
 	I > 0, !,
-	write('+---+---+---+---+---+---+---+---+---+'),nl,
-	draw_line(L),nl,
+	write('+---+---+---+---+---+---+---+---+---+'), nl,
+	draw_line(L), nl,
 	NI is I - 1,
-	display_board(T,NI).
+	display_board(T, NI).
 
-draw_line(L):-draw_line(L,9).
-draw_line(_,0):-write('|').
-draw_line([H|T],J):-
+draw_line(L):-draw_line(L, 9).
+draw_line(_, 0):-write('|').
+draw_line([H|T], J):-
 	J > 0, !,
-	write('| '),write(H),write(' '),
+	write('| '), write(H), write(' '),
 	NJ is J - 1,
-	draw_line(T,NJ).
+	draw_line(T, NJ).
 
-display_game(B,P):-
+display_game(B, P):-
     game_ui(P),
     display_board(B).
